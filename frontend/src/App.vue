86925a91459a03563a500cfa4607fb6d00a7ec70
Konstantin <levachin@luckandroll.com>
2019-12-20 09:33:33 +0300

    installed vue-bootstrap, font-awesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -27,6 +27,11 @@
 </script>
 
 <style lang="scss">
+
+    @import "~bootstrap/dist/css/bootstrap.css";
+    @import "~bootstrap-vue/dist/bootstrap-vue.css";
+    @import "~@fortawesome/fontawesome-free/css/all.css";
+
     body {
         min-height: 100vh;
     }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
