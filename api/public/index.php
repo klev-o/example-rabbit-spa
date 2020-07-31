4b6656d1be0df515cc051254829797fa9d9d6ec9
Konstantin <levachin@luckandroll.com>
2020-07-31 17:39:39 +0300

    Added api dotenv

--- api/public/index.php
+++ api/public/index.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 use Api\Http\Action\HomeAction;
 use Slim\App;
 use Slim\Container;
+use Symfony\Component\Dotenv\Dotenv;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
+
+if (file_exists('.env')) {
+    (new Dotenv())->load('.env');
+}
 
 $config = require 'config/config.php';
 $container = new Container($config);
