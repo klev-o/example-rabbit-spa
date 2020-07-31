cb723a481ae3d0e5ce2b1d0f203ed410de3fe2e0
Konstantin <levachin@luckandroll.com>
2020-07-31 20:43:01 +0300

    Added tests fixtures

--- api/tests/Feature/WebTestCase.php
+++ api/tests/Feature/WebTestCase.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Api\Test\Feature;
 
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\ServerRequest;
 use Laminas\Diactoros\Uri;
@@ -45,6 +49,23 @@ class WebTestCase extends TestCase
         $response = $this->app()->process($request, new Response());
         $response->getBody()->rewind();
         return $response;
+    }
+
+    protected function loadFixtures(array $fixtures): void
+    {
+        $container = $this->container();
+        $em = $container->get(EntityManagerInterface::class);
+        $loader = new Loader();
+        foreach ($fixtures as $class) {
+            if ($container->has($class)) {
+                $fixture = $container->get($class);
+            } else {
+                $fixture = new $class;
+            }
+            $loader->addFixture($fixture);
+        }
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
     }
 
     private function app(): App
