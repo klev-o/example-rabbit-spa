854c3ffada93a6da5b8baad3808e73af0b88a462
Konstantin <levachin@luckandroll.com>
2020-07-31 19:26:37 +0300

    Installed Doctrine migrations

--- api/bin/app.php
+++ api/bin/app.php
@@ -3,6 +3,9 @@
 
 declare(strict_types=1);
 
+use Doctrine\Migrations\Configuration\Configuration;
+use Doctrine\Migrations\Tools\Console\ConsoleRunner;
+use Doctrine\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
@@ -23,9 +26,19 @@ $container = require 'config/container.php';
 $cli = new Application('Application console');
 
 $entityManager = $container->get(EntityManagerInterface::class);
+
+$connection = $entityManager->getConnection();
+
+$configuration = new Configuration($connection);
+$configuration->setMigrationsDirectory('src/Data/Migration');
+$configuration->setMigrationsNamespace('Api\Data\Migration');
+
+
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
 
 Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
