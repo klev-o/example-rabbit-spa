a6f6f878322bdbc639c8591f88dcd5332376257e
Konstantin <levachin@luckandroll.com>
2020-07-31 19:18:17 +0300

    Enabled Doctrine commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -3,6 +3,8 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -19,6 +21,11 @@ if (file_exists('.env')) {
 $container = require 'config/container.php';
 
 $cli = new Application('Application console');
+
+$entityManager = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
