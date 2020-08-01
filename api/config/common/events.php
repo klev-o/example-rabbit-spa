6f62c44930050a7a57c6d7caeb1869b615582e2e
Konstantin <levachin@luckandroll.com>
2020-08-01 03:36:02 +0300

    Added user created listener

--- api/config/common/events.php
+++ api/config/common/events.php
@@ -5,12 +5,25 @@ declare(strict_types=1);
 use Api\Infrastructure\Model\EventDispatcher\Listener;
 use Api\Infrastructure\Model\EventDispatcher\SyncEventDispatcher;
 use Psr\Container\ContainerInterface;
+use Api\Model\User as UserModel;
 
 return [
     Api\Model\EventDispatcher::class => function (ContainerInterface $container) {
         return new SyncEventDispatcher(
             $container,
-            []
+            [
+                UserModel\Entity\User\Event\UserCreated::class => [
+                    Listener\User\CreatedListener::class,
+                ],
+            ]
         );
     },
+
+    Listener\User\CreatedListener::class => function (ContainerInterface $container) {
+        return new Listener\User\CreatedListener(
+            $container->get(Swift_Mailer::class),
+            $container->get('config')['mailer']['from']
+        );
+    },
+
 ];
