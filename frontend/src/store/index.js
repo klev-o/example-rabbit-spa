b6962a2a92ae8bdc75e8194c786b7c5b279821c4
Konstantin <levachin@luckandroll.com>
2020-08-07 21:54:20 +0300

    Added frontend login

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,19 +1,65 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-      currentEmail: null
-  },
-  mutations: {
-      changeCurrentEmail(state, email) {
+    state: {
+        currentEmail: null,
+        user: JSON.parse(localStorage.getItem('user')),
+    },
+    getters: {
+        isLoggedIn(state) {
+            return !!state.user;
+        }
+    },
+    mutations: {
+        changeCurrentEmail(state, email) {
           state.currentEmail = email;
-      }
-  },
-  actions: {
-  },
-  modules: {
-  }
+        },
+        login(state, user) {
+            state.user = user;
+        },
+        logout(state) {
+            state.user = null;
+        }
+    },
+    actions: {
+        login(context, data) {
+            return new Promise((resolve, reject) => {
+                context.commit('logout');
+                axios.post('/oauth/auth', {
+                    grant_type: 'password',
+                    username: data.username,
+                    password: data.password,
+                    client_id: 'app',
+                    client_secret: '',
+                    access_type: 'offline',
+                })
+                    .then(response => {
+                        const user = response.data;
+                        localStorage.setItem('user', JSON.stringify(user));
+                        axios.defaults.headers.common['Authorization'] = 'Bearer ' + user.access_token;
+                        context.commit('login', user);
+                        resolve(user)
+                    })
+                    .catch(error => {
+                        context.commit('logout');
+                        localStorage.removeItem('user');
+                        reject(error)
+                    })
+            })
+        },
+        logout(context) {
+            return new Promise((resolve) => {
+                context.commit('logout');
+                localStorage.removeItem('user');
+                delete axios.defaults.headers.common['Authorization'];
+                resolve()
+            });
+        }
+    },
+    modules: {
+    }
 })
